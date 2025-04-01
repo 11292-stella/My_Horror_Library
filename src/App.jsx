15c1aece5e0f044de3799e4aade0934473547552
11292-stella <stella.marucelli@gmail.com>
2025-04-01 15:35:57 +0200

    card generate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import MyNav from "./components/MyNav"
 import MyFooter from "./components/MyFooter"
+import AllTheBooks from "./components/AllTheBooks"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <MyNav />
-      <main className="flex-grow-1">qui ci metto la roba</main>
+      <main className="flex-grow-1 container mt-4">
+        <AllTheBooks />
+      </main>
       <MyFooter />
     </div>
   )
---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react"
+import HorrorB from "../horror.json"
+import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
+import { Col, Container, Row } from "react-bootstrap"
+
+class AllTheBooks extends Component {
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          {HorrorB.map((book) => (
+            <Col xs={12} sm={6} md={4} lg={2} key={book.asin} className="mb-3">
+              <Card>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>Prezzo: {book.price}€</Card.Text>
+                  <Card.Text>Categoria: {book.category}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    )
+  }
+}
+export default AllTheBooks
